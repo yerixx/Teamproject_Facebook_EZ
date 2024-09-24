930dcc6a718081d78bf6c2a868a11381dbbed2c3
yerixxz <yerixxz@gmail.com>
2024-09-24 17:53:38 +0900

    240924

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,21 +1,13 @@
-import React, { useContext } from "react";
-import Header from "../components/Header";
-import { DataStateContext } from "../App";
+import React from "react";
+import Header from "../components/common/Header";
+
 
 const Main = () => {
-  const data = useContext(DataStateContext);
-  const { users, posts } = data;
-  console.log(data);
-
   return (
     <div>
       <Header />
-      <h1>
-        {users[0]?.name.firstName}
-        {users[0]?.name.lastName}
-      </h1>
-      <h1>{posts[0]?.content}</h1>
-      main
+      <h1>정면돌파</h1>
+      <h1>화이팅</h1>
     </div>
   );
 };
