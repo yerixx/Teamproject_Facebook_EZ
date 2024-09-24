7fa3913462277214eced50d1cdf45cdd8559668a
yerixxz <yerixxz@gmail.com>
2024-09-24 20:18:12 +0900

    240924

--- src/components/Detail/Mypage.js
+++ src/components/Detail/Mypage.js
@@ -2,14 +2,24 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.h1`
-color:var(--color-facebookblue);
+/* color:var(--color-facebookblue);
+ */
 `
 
 const Mypage = () => {
   return (
-    <>
-      <Wrapper>Mypage 222</Wrapper>
-    </>
+      <Wrapper>
+        <section>
+          <div className='profile'>.
+            <div className='profileImg'></div>
+            <div className='profileCont'>
+              <h1>박예림</h1>
+              <p>8시간전</p>
+            </div>
+          </div>
+
+        </section>
+      </Wrapper>
   )
 }
 
