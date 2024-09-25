72495fae001a7aea09eacb81059203744054b4f4
yerixxz <yerixxz@gmail.com>
2024-09-25 23:00:47 +0900

    vite 개발환경 변경

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/common/Header";
+
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <h1>정면돌파</h1>
+      <h1>화이팅</h1>
+    </div>
+  );
+};
+
+export default Main;
