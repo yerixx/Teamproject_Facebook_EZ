2e41e06202e3912655fd4e9eb107e3c7fc801d35
ParkTH-Dev <xotj2635@naver.com>
2024-09-25 17:08:38 +0900

    first commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import Header from "../components/common/Header";
+import styled from "styled-components";
+import { HeaderBottom1, HeaderTop1 } from "../components/common/Header";
+import LeftSideBar from "../components/common/LeftSideBar";
+import MainContents from "../components/MainPage/MainContents";
 
-
+const Wrapper = styled.div`
+  height: 2000px;
+`;
+const MainSection = styled.section`
+  display: flex;
+  justify-content: space-between;
+  padding: 28px 20px;
+`;
 const Main = () => {
   return (
-    <div>
-      <Header />
-      <h1>정면돌파</h1>
-      <h1>화이팅</h1>
-    </div>
+    <Wrapper>
+      <header>
+        <HeaderTop1 />
+        <HeaderBottom1 />
+      </header>
+      <MainSection>
+        <LeftSideBar />
+        <MainContents />
+
+        <LeftSideBar />
+      </MainSection>
+    </Wrapper>
   );
 };
 
