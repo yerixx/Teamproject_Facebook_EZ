b3d5baf77cedaa1ed4033a1927beb70c5b9e62a7
seongjun1208 <skwid17@naver.com>
2024-09-25 17:27:48 +0900

    0925vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // BrowserRouter 임포트 추가
-import App from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // BrowserRouter 임포트 추가
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+//RE TEST //
