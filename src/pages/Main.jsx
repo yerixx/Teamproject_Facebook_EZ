6dac95b7d3e5b8d54ed36a6fb8e3f9de3cf354d0
김정하 <gimjeongha@gimjeonghaui-MacBookPro.local>
2024-09-26 19:30:42 +0900

    uploadpost

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 import { HeaderBottom1, HeaderTop1 } from "../components/common/Header";
-import LeftSideBar from "../components/common/LeftSideBar";
+// import LeftSideBar from "../components/common/LeftSideBar";
 import Mainpage from "../components/Main/Mainpage";
+import Mainupload from "../components/Main/Mainupload";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 2000px;
-`;
-
-const MainHeader = styled.h1`
-  ${MainHeaderStyle}
 `;
 
 const Main = () => {
@@ -17,6 +14,7 @@ const Main = () => {
     <Wrapper>
       <HeaderTop1 />
       <HeaderBottom1 />
+      <Mainupload />
       <Mainpage />
       <Mainpage />
     </Wrapper>
