025aeeb9fb12975d17065c51f4dd5c16dfac37a6
kyj102 <yain4746@naver.com>
2024-09-26 17:53:57 +0900

    test

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Mypage from "../components/Detail/Mypage";
+
 
 const Detail = () => {
   return <div>
-    <Mypage/>
+
   </div>;
 };
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Header from "../components/common/Header";
-import { MainHeaderStyle } from "../styles/GlobalStyles.styles";
+// import { MainHeader01Style } from "../styles/GlobalStyles.styles";
 import styled from "styled-components";
 
 
-const MainHeader = styled.h1`
-  ${MainHeaderStyle}
-`
+// const MainHeader = styled.h1`
+//   ${MainHeader01Style}
+// `
 
 const Main = () => {
   return (
     <div>
       <Header />
-      <MainHeader>정면돌파</MainHeader>
-      <h1>화이팅</h1>
     </div>
   );
 };
