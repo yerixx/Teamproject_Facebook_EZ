891703cf995cb3d544e16a0a275d3b599eb1048f
yerixxz <yerixxz@gmail.com>
2024-09-26 20:12:05 +0900

    Noto sans kr import 방법 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import Header from "../components/common/Header";
-import { MainHeaderStyle } from "../styles/GlobalStyles.styles";
-import styled from "styled-components";
-
-
-const MainHeader = styled.h1`
-  ${MainHeaderStyle}
-`
 
 const Main = () => {
   return (
     <div>
       <Header />
-      <MainHeader>정면돌파</MainHeader>
+      <div>정면돌파</div>
       <h1>화이팅</h1>
     </div>
   );
