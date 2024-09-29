056e4ad476680f0ff6a239a77cea33f86ddc736b
김정하 <gimjeongha@gimjeonghaui-MacBookPro.local>
2024-09-29 21:19:52 +0900

    Mainstory

--- src/components/Main/Mainstory.jsx
+++ src/components/Main/Mainstory.jsx
@@ -23,68 +23,84 @@ const Inner = styled.div`
   gap: 8px;
 `;
 
-const StoryItem = styled.div`
+const Storyitem = styled.div`
   width: 150px;
   height: 252px;
   border-radius: 8px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
   background: var(--color-light-gray-01);
   position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
-  padding-bottom: 8px;
-  .humanicon {
-    width: 100%;
-    height: 158px;
-    font-size: 167px;
-    color: var(--color-gray-01);
-    position: absolute;
-    top: 0;
-    right: 7px;
-  }
-  .plusicon {
-    border-radius: 50%;
-    background: var(--color-facebookblue);
-    width: 44px;
-    height: 44px;
-    border: 4px solid #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 25px;
-    color: #fff;
-    position: absolute;
-    top: 135px;
-    left: 55px;
-  }
-  .text {
-    margin-top: 5px;
-    font-size: 14px;
-    color: #333;
+  .mystory {
+    border-radius: 8px 8px 0 0;
+    .humanicon {
+      /* border: 1px solid red; */
+      width: 100%;
+      height: 158px;
+      font-size: 167px;
+      color: var(--color-gray-01);
+      position: absolute;
+      top: 0;
+      right: 7px;
+    }
+    .storymake {
+      .plusicon {
+        border-radius: 50%;
+        background: var(--color-facebookblue);
+        width: 44px;
+        height: 44px;
+        border: 4px solid #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 25px;
+        color: #fff;
+        position: absolute;
+        top: 135px;
+        left: 55px;
+        z-index: 1;
+      }
+    }
+    .textitem {
+      width: 100%;
+      height: 94px;
+      display: flex;
+      position: absolute;
+      top: 157px;
+      border-radius: 0 0 8px 8px;
+      background: #fff;
+    }
+    .text {
+      position: absolute;
+      top: 225px;
+      left: 35px;
+    }
   }
 `;
 
-const MainStory = () => {
+const Mainstory = () => {
   return (
     <Wrapper>
       <Inner>
-        <StoryItem>
-          <div className="humanicon">
-            <IoPersonSharp />
+        <Storyitem>
+          <div className="mystory">
+            <div className="humanicon">
+              <IoPersonSharp />
+            </div>
+            <div className="storymake">
+              <div className="plusicon">
+                <FaPlus />
+              </div>
+            </div>
+            <div className="textitem"></div>
+            <div className="text">스토리 만들기</div>
           </div>
-          <div className="plusicon">
-            <FaPlus />
-          </div>
-          <div className="text">스토리 만들기</div>
-        </StoryItem>
-        <StoryItem>
-          <div className="text">스토리</div>
-        </StoryItem>
+        </Storyitem>
+        <Storyitem>
+          <div className="mystory"></div>
+        </Storyitem>
       </Inner>
     </Wrapper>
   );
 };
 
-export default MainStory;
+export default Mainstory;
