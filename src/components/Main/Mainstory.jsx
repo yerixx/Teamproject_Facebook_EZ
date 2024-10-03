43baaed4d103311700fb7da3a881ad8fba3a288c
김정하 <gimjeongha@gimjeonghaui-MacBookPro.local>
2024-10-03 21:25:59 +0900

    slide

--- src/components/Main/Mainstory.jsx
+++ src/components/Main/Mainstory.jsx
@@ -1,95 +1,152 @@
-import React from "react";
 import styled from "styled-components";
-import { IoPersonSharp } from "react-icons/io5";
-import { FaPlus } from "react-icons/fa";
-
+import { FaUser } from "react-icons/fa6";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaPlus } from "react-icons/fa6";
+import { SubDescription_16_n } from "../../styles/GlobalStyles.styles";
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { MdOutlineNavigateBefore } from "react-icons/md";
 
 const Wrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
-  margin-top: 50px;
-  gap: 10px;
 
-  @media (max-width: 768px) {
-    margin-top: 30px;
+  margin-top: 20px;
+  @media screen and (max-width: 1050px) {
+    margin-top: 50px;
   }
-
-  @media (max-width: 390px) {
-    /* max-width: 100%; */
-    margin-top: 20px;
-    gap: 5px;
+  @media screen and (max-width: 768px) {
+    margin-top: 0;
+    height: 250px;
   }
 `;
 
 const Inner = styled.div`
   width: 1000px;
+  height: 360px;
+  padding: 27px 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  gap: 10px;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-  border-radius: 30px;
-
-  .inner {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-  }
-
-  @media (max-width: 768px) {
+  border-radius: var(--border-radius-30);
+  position: relative;
+  @media screen and (max-width: 1050px) {
     width: 100%;
-    padding: 20px;
   }
-
-  @media (max-width: 390px) {
-    border-radius: 0px;
+  @media screen and (max-width: 768px) {
+    box-shadow: none;
+    padding: 0;
+    width: 90vw;
+    height: 100%;
+    min-width: 360px;
+    margin: 0 auto;
+  }
+`;
+const Items = styled.div`
+  .slider {
+    width: 100%;
+    height: 100%;
+  }
+  .slick-slide {
+    margin: 0 5px;
+  }
+  .slick-list {
+    margin: 0 -10px;
+  }
+  .slick-track {
+    display: flex;
+    justify-content: flex-start;
+    transition: transform 0.5s ease-in-out;
   }
 `;
 
 const StoryItem = styled.div`
-  padding: 10px;
-  width: 150px;
-  height: 300px;
-  border-radius: 30px;
-  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-  .myStory {
-    border-radius: 30px;
-    height: 100%;
-    width: 100%;
-    position: relative;
-    background-color: #ddd;
+  border: 1px solid var(--color-light-gray-01);
+  width: 100%;
+  height: 320px;
+  border-radius: 8px;
+  position: relative;
 
-    /* 아이콘이나 텍스트 추가 */
+  div {
+    width: 100%;
+    height: 70%;
+    background-color: var(--color-light-gray-01);
+    position: relative;
+    svg {
+      color: var(--color-gray-01);
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      font-size: 160px;
+    }
+  }
+  span {
+    border: 5px solid #fff;
+    position: absolute;
+    background-color: var(--color-facebookblue);
+    width: 55px;
+    height: 55px;
+    border-radius: 50%;
     display: flex;
     justify-content: center;
     align-items: center;
-    color: #555;
-    font-size: 24px;
+    left: 50%;
+    top: 61%;
+    transform: translateX(-50%);
+    svg {
+      font-size: 30px;
+      color: #fff;
+    }
   }
-
-  @media (max-width: 768px) {
-    width: 38%;
-    height: 250px;
+  h2 {
+    ${SubDescription_16_n}
+    display: flex;
+    justify-content: center;
+    line-height: 8;
   }
-
-  @media (max-width: 390px) {
-    width: 150px;
-    height: 255px;
+  @media screen and (max-width: 768px) {
+    height: 200px;
+    div {
+      svg {
+        font-size: 100px;
+      }
+    }
+    span {
+      border: 3px solid #fff;
+      width: 34px;
+      height: 34px;
+      svg {
+        font-size: 16px;
+      }
+    }
+    h2 {
+      line-height: 5;
+    }
   }
 `;
 
 const StoryFriend = styled.div`
-  padding: 10px;
-  width: 150px;
-
+  width: 100%;
+  height: 320px;
+  background: #ddd;
+  border-radius: 8px;
+  position: relative;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
   .storyInfo {
     width: 100%;
-    position: relative;
-    img {
-      width: 100%;
-      border-radius: 30px;
-      opacity: 0.8;
-    }
 
     .story {
       width: 44px;
@@ -102,12 +159,12 @@ const StoryFriend = styled.div`
       display: flex;
       justify-content: center;
       align-items: center;
-      background: #fff;
+      /* background: #fff; */
 
       .storyProfile {
         width: 100%;
         height: 100%;
-        background: #aaa; /* var(--color-gray-01) 대체 */
+        background: #aaa;
         border-radius: 50%;
       }
     }
@@ -115,50 +172,114 @@ const StoryFriend = styled.div`
     .storyName {
       position: absolute;
       bottom: 0;
-      left: 0;
       width: 100%;
-      height: 36px;
+      height: 40px;
       background: rgba(0, 0, 0, 0.6);
       color: white;
       padding-left: 20px;
       line-height: 36px;
       font-size: 14px;
-      border-radius: 0 0 30px 30px;
+      border-radius: 0 0 8px 8px;
     }
   }
-
-  @media (max-width: 768px) {
-    width: 35%;
-    height: 250px;
-  }
-
-  @media (max-width: 390px) {
-    width: 150px;
-    height: 255px;
+  @media screen and (max-width: 768px) {
+    height: 200px;
   }
 `;
+// 슬릭슬라이더 커스텀 화살표
+const NextBtn = styled.span`
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: var(--color-light-gray-01);
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  font-size: 40px;
+  color: #fff;
+  cursor: pointer;
+  svg {
+    margin-left: 5px;
+  }
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const NextArrow = ({ onClick }) => {
+  return (
+    <NextBtn onClick={onClick}>
+      <MdOutlineNavigateNext />
+    </NextBtn>
+  );
+};
+// 슬릭슬라이더 커스텀 화살표
+const PrevBtn = styled.span`
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: var(--color-light-gray-01);
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  z-index: 1;
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  font-size: 40px;
+  color: #fff;
+  cursor: pointer;
+  svg {
+    margin-left: 3px;
+  }
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const PrevArrow = ({ onClick }) => {
+  return (
+    <PrevBtn onClick={onClick}>
+      <MdOutlineNavigateBefore />
+    </PrevBtn>
+  );
+};
 
 const MainStory = () => {
   const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true,
-    draggable: true,
-    fade: false,
-    arrows: true,
-    initialSlide: 0,
+    swipe: true,
+    nextArrow: <NextArrow />, // 화살표 버튼을 커스텀해서 사용
+    prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1050,
+        breakpoint: 1050, // 1024px 이하일 때
         settings: {
-          slidesToShow: 5,
-          arrows: true,
+          slidesToShow: 4, // 슬라이드를 2개만 보여줌
+          slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 768, // 600px 이하일 때
         settings: {
-          slidesToShow: 5,
+          slidesToShow: 4, // 슬라이드를 1개만 보여줌
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 580, // 600px 이하일 때
+        settings: {
+          slidesToShow: 3, // 슬라이드를 1개만 보여줌
+          slidesToScroll: 1,
         },
       },
     ],
@@ -167,59 +288,82 @@ const MainStory = () => {
   return (
     <Wrapper>
       <Inner>
-        <Slider className="inner" {...settings}>
-          <StoryItem>
-            <div className="myStory">
-              <IoPersonSharp /> {/* 아이콘 추가 */}
-              {/* 또는 원하는 콘텐츠 추가 */}
-            </div>
-          </StoryItem>
-          <StoryFriend>
-            <div className="storyInfo">
-              <img src="/img/test.jpg" alt="testimg" />
-              <div className="story">
-                <div className="storyProfile"></div>
+        <Items>
+          <Slider className="slider" {...settings}>
+            <StoryItem>
+              <div>
+                <FaUser />
               </div>
-              <div className="storyName">김정하</div>
-            </div>
-          </StoryFriend>
-          <StoryFriend>
-            <div className="storyInfo">
+              <span>
+                <FaPlus />
+              </span>
+              <h2>스토리 만들기</h2>
+            </StoryItem>
+            <StoryFriend>
               <img src="/img/test.jpg" alt="testimg" />
-              <div className="story">
-                <div className="storyProfile"></div>
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
               </div>
-              <div className="storyName">김정하</div>
-            </div>
-          </StoryFriend>
-          <StoryFriend>
-            <div className="storyInfo">
+            </StoryFriend>
+            <StoryFriend>
               <img src="/img/test.jpg" alt="testimg" />
-              <div className="story">
-                <div className="storyProfile"></div>
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
               </div>
-              <div className="storyName">김정하</div>
-            </div>
-          </StoryFriend>
-          <StoryFriend>
-            <div className="storyInfo">
+            </StoryFriend>
+            <StoryFriend>
               <img src="/img/test.jpg" alt="testimg" />
-              <div className="story">
-                <div className="storyProfile"></div>
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
               </div>
-              <div className="storyName">김정하</div>
-            </div>
-          </StoryFriend>
-          <StoryFriend>
-            <div className="storyInfo">
+            </StoryFriend>
+            <StoryFriend>
               <img src="/img/test.jpg" alt="testimg" />
-              <div className="story">
-                <div className="storyProfile"></div>
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
               </div>
-              <div className="storyName">김정하</div>
-            </div>
-          </StoryFriend>
-        </Slider>
+            </StoryFriend>
+            <StoryFriend>
+              <img src="/img/test.jpg" alt="testimg" />
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
+              </div>
+            </StoryFriend>
+            <StoryFriend>
+              <img src="/img/test.jpg" alt="testimg" />
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
+              </div>
+            </StoryFriend>
+            <StoryFriend>
+              <img src="/img/test.jpg" alt="testimg" />
+              <div className="storyInfo">
+                <div className="story">
+                  <div className="storyProfile"></div>
+                </div>
+                <div className="storyName">김정하</div>
+              </div>
+            </StoryFriend>
+          </Slider>
+        </Items>
       </Inner>
     </Wrapper>
   );
