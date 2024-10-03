41c8b005e7f392af993b0c2fb26ce277dff60963
yerixxz <yerixxz@gmail.com>
2024-10-03 23:59:50 +0900

    커버사진 및 프로필 사진 state적용 완료

--- src/components/Detail/ProfileCard.jsx
+++ src/components/Detail/ProfileCard.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useState } from 'react';
+import React,{ useRef, useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons"; 
 
 import styled from 'styled-components'
-// import { motion, AnimatePresence } from "framer-motion";
 
 //img
 import testCat from '/img/testcat.jpg';
@@ -35,20 +35,23 @@ const WrapperFrom = styled.form`
   /* border:1px solid #f00; */
   } 
   `
-const Profile = styled.div`
+const ProfileContain = styled.div`
   width:100%;
   display: flex;
-  .profileContent{
-    width:100%;
+  gap:20px;
+  /* border:1px solid #f00; */
+`
+const ProfileImgCont = styled.div`
+  position: relative; 
     display: flex;
     gap:30px;
   @media (max-width : 768px) {
-    width:100%;
     padding:0 20px ;
     align-items: center;
     gap:15px;
     } 
   .profileImg{
+    position: relative;
     width:100px;
     height:100px;
     background:var(--color-gray-01);
@@ -59,8 +62,30 @@ const Profile = styled.div`
       width:110px;
       height:110px;
   }
-}
-  .profileText{
+  }
+  .editIcon{
+      position: absolute;
+      width:16px;
+      height:16px;
+      bottom: 10px;
+      right:-10px;
+      padding:8px;
+      background:var(--color-white);
+      border-radius:50%;
+      box-shadow:var(--box-shadow-01);
+      color:var(--color-black);
+      z-index:1;
+      transition:all 0.3s;
+      &:hover{
+       opacity: 0.8;
+      }
+      cursor: pointer;
+      @media (max-width: 768px) {
+        right:10px;
+       }
+  }
+`
+const ProfileText = styled.div`
     width:100%;
     display: flex;
     flex-direction:column;
@@ -73,7 +98,6 @@ const Profile = styled.div`
       /* 미디어 쿼리 */
       @media (max-width : 768px) {
       align-items: center;
-      
       }
       .profileName{
         ${MainTitle_26_b}
@@ -87,12 +111,7 @@ const Profile = styled.div`
         }
       }
     }
-  }
-}
-  /* 미디어 쿼리 */
-  @media (max-width : 768px) {
-  width: 100%;
-  }
+  
 `
 const EditProfileDesc = styled.div`
       position: relative;
@@ -194,8 +213,9 @@ const Button = styled.div`
 
 const ProfileCard = () => {
   const [isEditing,setEditing] = useState(false);
-  const [profileImg,setProfileImg] = useState(false);
+  const [profileImg,setProfileImg] = useState(testCat);
   const [desc,setDesc] = useState("A Photographer @pylpic")
+  const fileRef = useRef(null)
 
   const onSubmit = async (e) => {
     e.preventDefault(); // 폼 제출 시 페이지 리로드 방지
@@ -206,7 +226,6 @@ const ProfileCard = () => {
       setInputText(""); // 입력 필드 초기화
     }, 1000);
   }
-
   const profileEdite = () => {
     setEditing(true)
   }
@@ -224,14 +243,34 @@ const ProfileCard = () => {
       setEditing(false)
     }
   }
-
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileUrl = URL.createObjectURL(file);
+      setProfileImg(fileUrl); // 선택한 파일로 아이콘 업데이트
+      
+    }
+    // if (file && file.length === 1) {
+    //   if (file[0].size > maxFileSize) {
+    //     alert("The Maximum Capacity that can be uploaded is 5MB");
+    //     return;
+    //   }
+    //   setFile(file[0]);
+    // }
+  }
+  const handleIconClick = () => {
+    fileRef.current.click();
+  }
 
   return (
     <WrapperFrom onSubmit={onSubmit}>
-          <Profile>
-              <div className='profileContent' >
-                <img src={profileImg ? null : testCat} className='profileImg'/>
-                <div className='profileText'>
+              <ProfileContain>
+                <ProfileImgCont>
+                  <img className='profileImg' src={profileImg} alt='profile' />
+                  <input ref={fileRef} onChange={handleImgChange} id='fileInput' type='file' accept='image/*' style={{ display: "none" }}/>
+                  <FontAwesomeIcon onClick={handleIconClick} className='editIcon' icon={faCamera} />
+                </ProfileImgCont>
+                <ProfileText>
                   <div className='profileTop'>
                     <h1 className='profileName'>박예림</h1>
                     <Button>
@@ -240,28 +279,20 @@ const ProfileCard = () => {
                     </Button>
                   </div>
                   {isEditing ? (
-                <EditProfileDesc> 
-                <div className='editBox'>
-                <textarea 
-                  value={desc}
-                  onChange={(e) => setDesc(e.target.value)}
-                  />
-                  <div className='editBtns'>
-                    <button onClick={editCencel} type='submit'>취소</button>
-                    <button onClick={editSave} type='submit'>확인</button>
-                  </div>
-                 </div>
-                  </EditProfileDesc>
-                    ):  
-                    (<ProfileDesc>
-                    {desc}
-                   </ProfileDesc>)
-                    }
-                </div>
-              </div>
-          </Profile>
-  
-    </WrapperFrom>
+                  <EditProfileDesc> 
+                    <div className='editBox'>
+                      <textarea value={desc} onChange={(e) => setDesc(e.target.value)}/>
+                      <div className='editBtns'>
+                      <button onClick={editCencel} type='submit'>취소</button>
+                      <button onClick={editSave} type='submit'>확인</button>
+                      </div>
+                    </div>
+                  </EditProfileDesc> 
+                  ):(<ProfileDesc> {desc} </ProfileDesc>)
+                  }
+                </ProfileText>
+              </ProfileContain>
+      </WrapperFrom>
   )
 }
 
--- src/components/Detail/TopCover.jsx
+++ src/components/Detail/TopCover.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 import ProfileCard from './ProfileCard';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons"; 
 
 import testCat from '/img/testcat.jpg';
 
@@ -28,8 +31,9 @@ const Inner = styled.article`
       max-width: 100%;
   }
 `
-const CoverImg =styled.img`
+const CoverImg = styled.img`
   position:relative;
+  background:var(--color-gray-01);
   width:100%;
   /* height 변경 금지 */
   height:550px;
@@ -40,12 +44,54 @@ const CoverImg =styled.img`
        height:300px;
   }
 `
+const EditIcon = styled.div`
+  position: relative;
+  .editIcon{
+  position: absolute;
+  width:16px;
+  height:16px;
+  bottom: 130px;
+  right:20px;
+  padding:8px;
+  background:var(--color-white);
+  border-radius:50%;
+  box-shadow:var(--box-shadow-01);
+  color:var(--color-black);
+  cursor: pointer;
+  transition:all 0.3s;
+  &:hover{
+    opacity: 0.6;
+  }
+  @media (max-width: 768px) {
+    bottom: 50px;
+  }
+}
 
+`
 const TopCover = () => {
+  const [coverImg,setCoverImg] = useState(testCat);
+  const fileRef = useRef(null)
+
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      const fileURL = URL.createObjectURL(file);
+      setCoverImg(fileURL)
+    }
+  }
+
+  const handleIconClick = () => {
+    fileRef.current.click();
+  }
+
   return (
     <Wrapper>
       <Inner>
-        <CoverImg src={testCat} alt="Test Cat"/>
+        <CoverImg src={coverImg} alt="Test Cat"/>
+        <input ref={fileRef} onChange={handleImgChange} id='fileInput' type='file' style={{display:"none"}}></input>
+        <EditIcon>
+          <FontAwesomeIcon onClick={handleIconClick} className='editIcon' icon={faCamera} />
+        </EditIcon>
         <ProfileCard />
       </Inner>
     </Wrapper>
